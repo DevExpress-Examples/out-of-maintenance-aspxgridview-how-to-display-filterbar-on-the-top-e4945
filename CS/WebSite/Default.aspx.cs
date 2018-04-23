09ae348da87f255e3fdcd9f2375fe0636f084d9e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void ASPxGridView1_CustomFilterExpressionDisplayText(object sender, DevExpress.Web.CustomFilterExpressionDisplayTextEventArgs e) {
+        ASPxLabel lbl = ASPxGridView1.FindTitleTemplateControl("ASPxLabel1") as ASPxLabel;
+        lbl.Text = e.DisplayText;
+
+        Control c = lbl.Parent;
+
+        if (e.DisplayText != String.Empty) {
+            c.Controls.AddAt(1, new LiteralControl("<a style=\"font-size:small\" href=\"#\" onclick=\"grid.ApplyFilter();\">Clear</a>&nbsp::&nbsp"));
+        } else {
+            c.Controls.RemoveAt(1);
+        }
+    }
+}
